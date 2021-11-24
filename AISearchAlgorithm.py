f0513bb26182065fa6c4f8a2ec5ef57eee79e879
Seyyed Ali Shohadaalhosseini <al.shohadaee@gmail.com>
2021-11-24 21:57:10 +0330

    first commit

---
+++ AISearchAlgorithm.py
@@ -0,0 +1,33 @@
+# 										Writer of this code:
+# 								   Seyyed Ali Shohadaalhosseini
+
+#                                    We'll never forget that:
+#
+# 			        	 < What doesn't kill you makes you STRONGER >
+
+class SearchAlgorithm():
+    """
+        This module contains all the AI Search Algorithm.
+        Just import and call the method you want with the
+        required arguments.
+    """
+    def __init__(self):
+        pass
+
+    def searchAstar(self):
+        pass
+
+    def searchGreedy(self):
+        pass
+
+    def searchUCS(self):
+        pass
+
+    def searchBFS(self):
+        pass
+
+    def searchDFS(self):
+        pass
+
+    def searchIDS(self):
+        pass
